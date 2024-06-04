6d4a109a478ef764271d8feab1064df6b0402036
Luke Alexander <lukealexander2a@gmail.com>
2024-06-04 13:51:31 +0100

    feat: Refactor home page to use MapPage and MapProvider
    The code changes involve refactoring the home page to use the `MapPage` widget and `MapProvider` for state management. This change improves the organization and separation of concerns in the codebase.

--- frontend/lib/pages/map.dart
+++ frontend/lib/pages/map.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/states/map_state.dart';
+import 'package:frontend/widgets/base_widget.dart';
+import 'package:frontend/widgets/mapDrawer.dart';
 import 'package:map/map.dart';
-import 'package:latlng/latlng.dart';
 import 'dart:math';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/gestures.dart';
+import 'package:provider/provider.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -13,18 +16,6 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapState extends State<MapPage> {
-  final controller = MapController(
-    location: const LatLng(Angle.degree(35.675), Angle.degree(51.41)),
-    zoom: 2,
-  );
-
-  final markers = [
-    const LatLng(Angle.degree(35.674), Angle.degree(51.41)),
-    const LatLng(Angle.degree(5.678), Angle.degree(51.41)),
-    const LatLng(Angle.degree(25.682), Angle.degree(51.41)),
-    const LatLng(Angle.degree(36.686), Angle.degree(51.41)),
-  ];
-
   Widget _buildMarkerWidget(Offset pos, Color color,
       [IconData icon = Icons.location_on]) {
     return Positioned(
@@ -57,7 +48,8 @@ class _MapState extends State<MapPage> {
     _scaleStart = 1.0;
   }
 
-  void _onScaleUpdate(ScaleUpdateDetails details, MapTransformer transformer) {
+  void _onScaleUpdate(ScaleUpdateDetails details, MapTransformer transformer,
+      MapController controller) {
     final scaleDiff = details.scale - _scaleStart;
     _scaleStart = details.scale;
 
@@ -85,61 +77,87 @@ class _MapState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MapLayout(
-      controller: controller,
-      builder: (context, transformer) {
-        final markerPositions = markers.map(transformer.toOffset).toList();
+    return Scaffold(
+      body: BaseWidget<MapState>(
+        state: Provider.of<MapState>(context),
+        onStateReady: (state) async {
+          await state.getMarkers();
+          WidgetsBinding.instance.addPostFrameCallback((_) {
+            if (mounted) {
+              setState(() {});
+            }
+          });
+        },
+        builder: (context, state, child) {
+          return SafeArea(
+            child: Scaffold(
+              appBar: AppBar(
+                title: const Text('Map Page'),
+              ),
+              drawer: const mapDrawer(),
+              body: MapLayout(
+                controller: state.controller,
+                builder: (context, transformer) {
+                  final markerPositions =
+                      state.markers.map(transformer.toOffset).toList();
 
-        final markerWidgets = markerPositions.map(
-          (pos) => _buildMarkerWidget(pos, Colors.red),
-        );
-        return GestureDetector(
-          behavior: HitTestBehavior.opaque,
-          onScaleStart: _onScaleStart,
-          onScaleUpdate: (details) => _onScaleUpdate(details, transformer),
-          child: Listener(
-            behavior: HitTestBehavior.opaque,
-            onPointerSignal: (event) {
-              if (event is PointerScrollEvent) {
-                final delta = event.scrollDelta.dy / -1000.0;
-                final zoom = clamp(controller.zoom + delta, 2, 18);
+                  final markerWidgets = markerPositions.map(
+                    (pos) => _buildMarkerWidget(pos, Colors.red),
+                  );
+                  return GestureDetector(
+                    behavior: HitTestBehavior.opaque,
+                    onScaleStart: _onScaleStart,
+                    onScaleUpdate: (details) =>
+                        _onScaleUpdate(details, transformer, state.controller),
+                    child: Listener(
+                      behavior: HitTestBehavior.opaque,
+                      onPointerSignal: (event) {
+                        if (event is PointerScrollEvent) {
+                          final delta = event.scrollDelta.dy / -1000.0;
+                          final zoom =
+                              clamp(state.controller.zoom + delta, 2, 18);
 
-                transformer.setZoomInPlace(zoom, event.localPosition);
-                setState(() {});
-              }
-            },
-            child: Stack(
-              children: [
-                TileLayer(
-                  builder: (context, x, y, z) {
-                    final tilesInZoom = pow(2.0, z).floor();
+                          transformer.setZoomInPlace(zoom, event.localPosition);
+                          setState(() {});
+                        }
+                      },
+                      child: Stack(
+                        children: [
+                          TileLayer(
+                            builder: (context, x, y, z) {
+                              final tilesInZoom = pow(2.0, z).floor();
 
-                    while (x < 0) {
-                      x += tilesInZoom;
-                    }
-                    while (y < 0) {
-                      y += tilesInZoom;
-                    }
+                              while (x < 0) {
+                                x += tilesInZoom;
+                              }
+                              while (y < 0) {
+                                y += tilesInZoom;
+                              }
 
-                    x %= tilesInZoom;
-                    y %= tilesInZoom;
+                              x %= tilesInZoom;
+                              y %= tilesInZoom;
 
-                    //Google Maps
-                    final url =
-                        'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
+                              //Google Maps
+                              final url =
+                                  'https://www.google.com/maps/vt/pb=!1m4!1m3!1i$z!2i$x!3i$y!2m3!1e0!2sm!3i420120488!3m7!2sen!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0!23i4111425';
 
-                    return CachedNetworkImage(
-                      imageUrl: url,
-                      fit: BoxFit.cover,
-                    );
-                  },
-                ),
-                ...markerWidgets,
-              ],
+                              return CachedNetworkImage(
+                                imageUrl: url,
+                                fit: BoxFit.cover,
+                              );
+                            },
+                          ),
+                          ...markerWidgets,
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
             ),
-          ),
-        );
-      },
+          );
+        },
+      ),
     );
   }
 }
