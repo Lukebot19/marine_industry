6e5c768fb5bc23bd9c8f9cc835d8f67a028da508
Luke Alexander <lukealexander2a@gmail.com>
2024-06-04 16:17:51 +0100

    feat: Remove unused code and update API URLs
    This commit removes the unused `views.py` file in the `vessels` app. Additionally, it updates the API URLs in the `urls.py` file to follow the recommended format and structure. These changes improve code cleanliness and ensure that the API endpoints are correctly configured.

---
+++ backend/backend/vessels/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import SimpleRouter
+
+from . import viewsets
+
+router = SimpleRouter()
+# router.register(r"vessels", viewsets.VesselViewSet, basename="vessels")
+
+# URL Patterns is kept for backwards compatibility with certain Django functionality.
+urlpatterns = [
+    path("", include(router.urls)),
+]
