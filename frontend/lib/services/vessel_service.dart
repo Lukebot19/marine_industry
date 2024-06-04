fd25718c6a30d4e1b4f059e0ec298c166649c5f7
Luke Alexander <lukealexander2a@gmail.com>
2024-06-04 15:49:07 +0100

    Update a vessel

--- frontend/lib/services/vessel_service.dart
+++ frontend/lib/services/vessel_service.dart
@@ -7,12 +7,13 @@ import '../API_config.dart';
 
 class VeesselService {
 
+  // Get all vessels
   Future<List<Vessel>> getVessels() async {
     APIConfig config = APIConfig();
     if (config.development) {
       return Vessel.dummyData;
     }
-    
+
     final response = await http.get(Uri.parse('${config.API_URL}/vessels/'));
 
     if (response.statusCode == 200) {
@@ -23,4 +24,25 @@ class VeesselService {
       throw Exception('Failed to load vessels');
     }
   }
+
+  // Update a vessel
+  Future<void> updateVessel(Vessel vessel) async {
+    APIConfig config = APIConfig();
+    if (config.development) {
+      return;
+    }
+
+    final response = await http.put(
+      Uri.parse('${config.API_URL}/vessels/${vessel.id}/'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(vessel.toMap()),
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to update vessel');
+    }
+  }
+
 }
