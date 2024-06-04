7f4f4a7f11225d9dd4b0806ee0b5621175d67242
Luke Alexander <lukealexander2a@gmail.com>
2024-06-04 16:38:53 +0100

    feat: Enable vessel addition on the map
    The code changes in `vessel_service.dart` and `map_state.dart` allow users to add vessels directly on the map. The `addVessel` method in `VesselService` now accepts the `name`, `longitude`, and `latitude` parameters to create a new vessel. In `MapState`, the `addVessel` method now calls the `addVessel` method from `VesselService` and adds the returned vessel to the list of vessels. This enhancement improves the user experience by enabling vessel addition functionality on the map.

--- backend/backend/vessels/urls.py
+++ backend/backend/vessels/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import SimpleRouter
 from . import viewsets
 
 router = SimpleRouter()
-# router.register(r"vessels", viewsets.VesselViewSet, basename="vessels")
+router.register(r"vessels", viewsets.VesselViewSet, basename="vessels")
 
 # URL Patterns is kept for backwards compatibility with certain Django functionality.
 urlpatterns = [
