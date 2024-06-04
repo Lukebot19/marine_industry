d1d34c8cf0a8b63c6e75031293d40c515df5dda5
Luke Alexander <lukealexander2a@gmail.com>
2024-06-04 15:08:03 +0100

    Add sort

--- frontend/lib/widgets/mapDrawer.dart
+++ frontend/lib/widgets/mapDrawer.dart
@@ -2,13 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:frontend/states/map_state.dart';
 import 'package:provider/provider.dart';
 
-class mapDrawer extends StatelessWidget {
+class mapDrawer extends StatefulWidget {
   const mapDrawer({
     super.key,
   });
 
   @override
+  _mapDrawerState createState() => _mapDrawerState();
+}
+
+class _mapDrawerState extends State<mapDrawer> {
+  String filter = '';
+  bool isAscending = true;
+
+  @override
   Widget build(BuildContext context) {
+    var vessels = Provider.of<MapState>(context).vessels;
+    var filteredVessels = vessels
+        .where((vessel) =>
+            vessel.name.toLowerCase().contains(filter.toLowerCase()))
+        .toList();
+
+    filteredVessels.sort((a, b) {
+      return isAscending ? a.name.compareTo(b.name) : b.name.compareTo(a.name);
+    });
+
     return Drawer(
       child: Stack(
         children: <Widget>[
@@ -19,12 +37,40 @@ class mapDrawer extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: Colors.blue,
                 ),
-                child: Text('Drawer Header'),
+                child: Text('Vessel Menu'),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: TextField(
+                        onChanged: (value) {
+                          setState(() {
+                            filter = value;
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          labelText: "Search",
+                          prefixIcon: Icon(Icons.search),
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isAscending = !isAscending;
+                        });
+                      },
+                      icon: const Icon(Icons.filter_list),
+                    ),
+                  ],
+                ),
               ),
               Provider.of<MapState>(context).isLoading == false
                   ? Column(
                       children: [
-                        ...Provider.of<MapState>(context).vessels.map(
+                        ...filteredVessels.map(
                           (vessel) {
                             return ListTile(
                               title: Text(vessel.name),
@@ -42,7 +88,7 @@ class mapDrawer extends StatelessWidget {
             ],
           ),
           if (Provider.of<MapState>(context).isLoading)
-            Center(child: CircularProgressIndicator()),
+            const Center(child: CircularProgressIndicator()),
         ],
       ),
     );
